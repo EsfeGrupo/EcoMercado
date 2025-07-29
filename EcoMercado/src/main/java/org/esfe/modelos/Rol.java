21299baa659ed0efb6ffa4b6cf526c28cea4a455
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-07-29 11:39:54 -0600

    se creo la tabla usuario y se añadio la relacion m a 1 con rol

--- EcoMercado/src/main/java/org/esfe/modelos/Rol.java
+++ EcoMercado/src/main/java/org/esfe/modelos/Rol.java
@@ -3,6 +3,9 @@ package org.esfe.modelos;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "Rol")
 public class Rol {
@@ -15,6 +18,20 @@ public class Rol {
 
     @NotBlank(message = "La descripción del rol es requerido")
     private String descripcion;
+
+    @OneToMany(mappedBy = "rol", // "rol" es el nombre del campo en la entidad Usuario que mapea esta relación
+            fetch = FetchType.LAZY // Cuando cargues un Rol, los Usuarios no se cargarán hasta que los pidas.
+            // LAZY es el valor por defecto para OneToMany y es recomendado para rendimiento.
+    )
+    private List<Usuario> usuarios = new ArrayList<>();
+
+    public List<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(List<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
 
     public Integer getId() {
         return id;
--- EcoMercado/src/main/java/org/esfe/modelos/Usuario.java
+++ EcoMercado/src/main/java/org/esfe/modelos/Usuario.java
@@ -1,4 +1,77 @@
 package org.esfe.modelos;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Usuario")
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "El nombre de usuario es requerido")
+    private String nombre;
+
+    @NotBlank(message = "El correo de usuario es requerido")
+    private String correo;
+
+    @NotBlank(message = "La contraseña es requerida")
+    private String password;
+
+    private int estado;
+
+    @ManyToOne(fetch = FetchType.EAGER) // El rol se carga inmediatamente con el usuario
+    @JoinColumn(name = "idRol", nullable = false)
+    private Rol rol;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
 }
