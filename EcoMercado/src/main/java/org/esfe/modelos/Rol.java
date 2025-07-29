383da64c6cb892ff719d70b2d59befb2334785bf
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-07-29 10:13:38 -0600

    se agregaron los get and set a rol

--- EcoMercado/src/main/java/org/esfe/modelos/Rol.java
+++ EcoMercado/src/main/java/org/esfe/modelos/Rol.java
@@ -16,4 +16,27 @@ public class Rol {
     @NotBlank(message = "La descripción del rol es requerido")
     private String descripcion;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
