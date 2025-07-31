c1101bdfb002e033d5eff8de9e532f4be321ae81
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-07-31 09:58:04 -0600

    creacion del script -Fran

---
+++ EcoMercado/src/main/java/org/esfe/modelos/Script.sql
@@ -0,0 +1,5 @@
+INSERT INTO rol (nombre, descripcion) VALUES ('admin','Administrador');
+INSERT INTO rol (nombre, descripcion) VALUES ('consumidor', 'rol del consumidor');
+-- clave 12345
+INSERT INTO usuario (nombre, correo, password, estado) VALUES ('Brayan','sysadmin', '$2a$12$r74HSGhuNB5zqfLG3fiao.OlRzKsPv/6R5EuhLNeFqjkKM7BZJ20m', 1);
+
