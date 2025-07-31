dddd04f4a5e7b2bf2b16956c48b5507f2941890d
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-07-31 10:17:57 -0600

    se creo el Repository de rol

---
+++ EcoMercado/src/main/java/org/esfe/repositorios/IRolRepository.java
@@ -0,0 +1,10 @@
+package org.esfe.repositorios;
+
+import org.esfe.modelos.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface IRolRepository extends JpaRepository<Rol, Integer> {
+    Page<Rol> findByNombreContainingAndDescripcionContaining(String nombre,String descripcion,Pageable pageable);
+}
