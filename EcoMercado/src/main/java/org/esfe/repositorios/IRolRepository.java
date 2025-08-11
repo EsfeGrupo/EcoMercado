e7415b06bedf8c558b69b796b8f69b74ba630113
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-08-11 09:00:37 -0600

    se añadio el codigo de ITarjetaCreditoRepository crud, busqueda, fechaexpiracion

--- EcoMercado/src/main/java/org/esfe/repositorios/IRolRepository.java
+++ EcoMercado/src/main/java/org/esfe/repositorios/IRolRepository.java
@@ -1,9 +1,10 @@
 package org.esfe.repositorios;
 
 import org.esfe.modelos.Rol;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Page;
-c
+
 
 public interface IRolRepository extends JpaRepository<Rol, Integer> {
     Page<Rol> findByNombreContainingIgnoreCaseAndDescripcionContainingIgnoreCase(String nombre, String descripcion, Pageable pageable);
