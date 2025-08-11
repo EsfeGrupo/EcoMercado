a8da3dcd746c4c16535b27af86fedd58f5e1af1d
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-08-11 08:57:09 -0600

    se añadio el codigo de ITarjetaCreditoRepository crud, busqueda, fechaexpiracion

---
+++ EcoMercado/src/main/java/org/esfe/repositorios/ITarjetaCreditoRepository.java
@@ -0,0 +1,15 @@
+package org.esfe.repositorios;
+
+import org.esfe.modelos.TarjetaCredito;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface ITarjetaCreditoRepository extends JpaRepository<TarjetaCredito, Integer> {
+    Page<TarjetaCredito> findByNumeroContainingIgnoreCaseAndNombreTitularContainingIgnoreCaseAndBancoContainingIgnoreCase(String numero, String nombreTitular, String banco, Pageable pageable);
+
+    List<TarjetaCredito> findByFechaExpiracionBefore(LocalDate fechaActual);
+}
