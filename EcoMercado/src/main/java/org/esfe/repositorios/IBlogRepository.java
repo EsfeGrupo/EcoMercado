1b79d50a723d41a94cc9bf4cf1fa13a39b926e07
Francisco Medina <shiro54828@gmail.com>
2025-08-11 08:21:46 -0600

    edición de IBlogRepo 2

--- EcoMercado/src/main/java/org/esfe/repositorios/IBlogRepository.java
+++ EcoMercado/src/main/java/org/esfe/repositorios/IBlogRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IBlogRepository extends JpaRepository<Blog, Integer> {
-    Page<Blog> findByAutorContainingAndDescripcionContaining(String autor, String descripcion, Pageable pageable);
+    Page<Blog> findByAutorContainingIgnoreCaseAndDescripcionContainingIgnoreCase(String autor, String descripcion, Pageable pageable);
 }
