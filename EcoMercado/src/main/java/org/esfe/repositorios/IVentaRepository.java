3116cc5bac1b3fc63b1e9d498d026a3719b8efa1
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-08-12 07:46:39 -0600

    se añadio orderby a IVentaRepository

--- EcoMercado/src/main/java/org/esfe/repositorios/IVentaRepository.java
+++ EcoMercado/src/main/java/org/esfe/repositorios/IVentaRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IVentaRepository extends JpaRepository<Venta, Integer> {
-    Page<Venta> findByCorrelativoContainingIgnoreCaseAndEstadoAndUsuario_IdAndTipoPago_IdAndTarjetaCredito_Id(String correlativo, Byte estado, Integer idUsuario, Integer idTipoPago, Integer idTarjetaCredito, Pageable pageable);
+    Page<Venta> findByCorrelativoContainingIgnoreCaseAndEstadoAndUsuario_IdAndTipoPago_IdAndTarjetaCredito_IdOrderByIdDesc(String correlativo, Byte estado, Integer idUsuario, Integer idTipoPago, Integer idTarjetaCredito, Pageable pageable);
 }
