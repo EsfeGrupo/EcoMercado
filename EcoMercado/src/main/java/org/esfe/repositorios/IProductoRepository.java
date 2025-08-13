07e20642b234c2eff64ab7c146287d463fcc5401
Fran-mndz <shiro54828@gmail.com>
2025-08-13 09:01:47 -0600

    Edicion de Producto para usar Precio como un List

--- EcoMercado/src/main/java/org/esfe/repositorios/IProductoRepository.java
+++ EcoMercado/src/main/java/org/esfe/repositorios/IProductoRepository.java
@@ -5,23 +5,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface IProductoRepository extends JpaRepository <Producto, Integer> {
 
     // Filtrar por nombre
-    Page<Producto> findByNombreContainingIgnoreCaseAndPrecio(
+    Page<Producto> findByNombreContainingIgnoreCaseAndPrecioIn(
             String nombre,
-            Float precio,
+            List<Float> precio,
             Pageable pageable
     );
-/*
-    // Filtrar por nombre que contenga y precio mayor que
-    Page<Producto> findByPrecioGreaterThan(
-            Float precio,
-            Pageable pageable
-    );
-    // Filtrar por nombre que contenga y precio menor que
-    Page<Producto> findByPrecioLessThan(
-            Float precio,
-            Pageable pageable
-    );*/
+
 }
