2664da6268a7b94a48d629d1ad8ef0a1d33288a0
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-08-14 10:45:01 -0600

    solo se creo el archivo de RolService

---
+++ EcoMercado/src/main/java/org/esfe/servicios/interfaces/ITarjetaCreditoService.java
@@ -0,0 +1,22 @@
+package org.esfe.servicios.interfaces;
+
+import org.esfe.modelos.TarjetaCredito;
+import org.esfe.modelos.Usuario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface ITarjetaCreditoService {
+    Page<TarjetaCredito> obtenerTodosPaginados(Pageable pageable);
+
+    List<TarjetaCredito> obtenerTodos();
+
+    Page<TarjetaCredito> findByNumeroContainingIgnoreCaseAndNombreTitularContainingIgnoreCaseAndBancoContainingIgnoreCaseOrderByIdDesc(String numero, String nombreTitular, String banco, Pageable pageable);
+
+    TarjetaCredito obtenerPorId(Integer id);
+
+    TarjetaCredito crearOEditar(TarjetaCredito tarjetaCredito);
+
+    void eliminarPorId(Integer id);
+}
