ec9510432d697ee9ff4643b04df4571caabefce2
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-08-14 11:45:12 -0600

    se añadio RolService en la carpeta de servicios.implementaciones tiene CRUD y buscar

--- EcoMercado/src/main/java/org/esfe/servicios/implementaciones/RolService.java
+++ EcoMercado/src/main/java/org/esfe/servicios/implementaciones/RolService.java
@@ -1,4 +1,47 @@
 package org.esfe.servicios.implementaciones;
 
-public class RolService {
+import org.esfe.modelos.Rol;
+import org.esfe.repositorios.IRolRepository;
+import org.esfe.servicios.interfaces.IRolService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RolService implements IRolService {
+    @Autowired
+    private IRolRepository rolRepository;
+
+    @Override
+    public Page<Rol> obtenerTodosPaginados(Pageable pageable) {
+        return rolRepository.findAll(pageable);
+    }
+
+    @Override
+    public List<Rol> obtenerTodos() {
+        return rolRepository.findAll();
+    }
+
+    @Override
+    public Page<Rol> findByNombreContainingIgnoreCaseAndDescripcionContainingIgnoreCaseOrderByIdDesc(String nombre, String descripcion, Pageable pageable) {
+        return rolRepository.findByNombreContainingIgnoreCaseAndDescripcionContainingIgnoreCaseOrderByIdDesc(nombre, descripcion, pageable);
+    }
+
+    @Override
+    public Rol obtenerPorId(Integer id) {
+        return rolRepository.findById(id).get();
+    }
+
+    @Override
+    public Rol crearOEditar(Rol rol) {
+        return rolRepository.save(rol);
+    }
+
+    @Override
+    public void eliminarPorId(Integer id) {
+        rolRepository.deleteById(id);
+    }
 }
