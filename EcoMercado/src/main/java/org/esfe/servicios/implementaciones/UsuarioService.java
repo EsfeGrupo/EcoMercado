8bd42e61a3d75dec6347dd1129da1644a197d769
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-08-15 10:31:12 -0600

    se creo UsuarioService servicios.implementaciones crud y busqueda

---
+++ EcoMercado/src/main/java/org/esfe/servicios/implementaciones/UsuarioService.java
@@ -0,0 +1,49 @@
+package org.esfe.servicios.implementaciones;
+import org.esfe.servicios.interfaces.IUsuarioService;
+import org.esfe.modelos.Usuario;
+import org.esfe.repositorios.IUsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioService implements IUsuarioService {
+    @Autowired
+    private IUsuarioRepository usuarioRepository;
+
+    @Override
+    public Page<Usuario> obtenerTodosPaginados(Pageable pageable) {
+        return usuarioRepository.findAll(pageable);
+    }
+
+    @Override
+    public List<Usuario> obtenerTodos() {
+        return usuarioRepository.findAll();
+    }
+
+    @Override
+    public Page<Usuario> findByNombreContainingIgnoreCaseAndCorreoContainingIgnoreCaseAndRol_IdOrderByIdDesc(
+            String nombre, String correo, Integer idRol, Pageable pageable) {
+        // TODO Auto-generated method stub
+        return usuarioRepository.findByNombreContainingIgnoreCaseAndCorreoContainingIgnoreCaseAndRol_IdOrderByIdDesc(nombre, correo, idRol, pageable);
+    }
+
+    @Override
+    public Usuario obtenerPorId(Integer id) {
+        Optional<Usuario> usuarioOptional = usuarioRepository.findById(id);
+        return usuarioOptional.orElse(null);
+    }
+
+    @Override
+    public Usuario crearOEditar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @Override
+    public void eliminarPorId(Integer id) {
+        usuarioRepository.deleteById(id);
+    }
+}
